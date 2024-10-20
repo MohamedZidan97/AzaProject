175901d9100d1206dcc8100285c5f1788c0826d1
MohamedZidan97 <mohamedzidan6846@gmail.com>
2024-10-20 20:57:41 +0300

    add commit text here

--- InventoryManagementSystem/Controllers/Ho5meController1.cs
+++ InventoryManagementSystem/Controllers/Ho5meController1.cs
@@ -4,6 +4,10 @@ namespace InventoryManagementSystem.Controllers
 {
     public class Ho5meController1 : Controller
     {
+        public class add
+        {
+            public int id { get; set; }
+        }
         public IActionResult Index()
         {
             return View();
